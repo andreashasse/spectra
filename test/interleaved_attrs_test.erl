2ab4580954ced9e584bcf49240377d4b5eaa02c2
Andreas Hasselberg <andreas.hasselberg@gmail.com>
2026-02-02 21:26:11 +0100

    the spectra attribute is matched with the next type / record even if other attributes are in between

---
+++ test/interleaved_attrs_test.erl
@@ -0,0 +1,96 @@
+-module(interleaved_attrs_test).
+
+-include_lib("eunit/include/eunit.hrl").
+
+interleaved_attrs_test() ->
+    Code =
+        "-module(test_interleaved).\n"
+        "-compile([nowarn_unused_type, nowarn_unused_record]).\n"
+        "-spectra(#{title => <<\"Type with doc attr\">>}).\n"
+        "-doc \"Some erlang doc\".\n"
+        "-type my_type() :: integer().\n"
+        "-spectra(#{title => <<\"Record with doc attr\">>}).\n"
+        "-doc \"Record documentation\".\n"
+        "-record(my_record, {field :: integer()}).\n",
+
+    {ok, test_interleaved, BeamBinary} = compile:forms(
+        parse_module(Code),
+        [binary, return_errors, debug_info]
+    ),
+
+    TempFile = "/tmp/test_interleaved.beam",
+    ok = file:write_file(TempFile, BeamBinary),
+
+    TypeInfo = spectra_abstract_code:types_in_module_path(TempFile),
+
+    {ok, TypeDoc} = spectra_type_info:find_doc(TypeInfo, my_type, 0),
+    ?assertEqual(#{title => <<"Type with doc attr">>}, TypeDoc),
+
+    {ok, RecordDoc} = spectra_type_info:find_record_doc(TypeInfo, my_record),
+    ?assertEqual(#{title => <<"Record with doc attr">>}, RecordDoc),
+
+    file:delete(TempFile).
+
+only_doc_no_spectra_test() ->
+    Code =
+        "-module(test_only_doc).\n"
+        "-compile([nowarn_unused_type]).\n"
+        "-doc \"Just erlang doc\".\n"
+        "-type my_type() :: integer().\n",
+
+    {ok, test_only_doc, BeamBinary} = compile:forms(
+        parse_module(Code),
+        [binary, return_errors, debug_info]
+    ),
+
+    TempFile = "/tmp/test_only_doc.beam",
+    ok = file:write_file(TempFile, BeamBinary),
+
+    TypeInfo = spectra_abstract_code:types_in_module_path(TempFile),
+
+    error = spectra_type_info:find_doc(TypeInfo, my_type, 0),
+
+    file:delete(TempFile).
+
+spectra_before_wrong_type_test() ->
+    Code =
+        "-module(test_wrong_type).\n"
+        "-compile([nowarn_unused_type]).\n"
+        "-spectra(#{title => <<\"Should be on first\">>}).\n"
+        "-type first() :: integer().\n"
+        "-type second() :: binary().\n",
+
+    {ok, test_wrong_type, BeamBinary} = compile:forms(
+        parse_module(Code),
+        [binary, return_errors, debug_info]
+    ),
+
+    TempFile = "/tmp/test_wrong_type.beam",
+    ok = file:write_file(TempFile, BeamBinary),
+
+    TypeInfo = spectra_abstract_code:types_in_module_path(TempFile),
+
+    {ok, FirstDoc} = spectra_type_info:find_doc(TypeInfo, first, 0),
+    ?assertEqual(#{title => <<"Should be on first">>}, FirstDoc),
+
+    error = spectra_type_info:find_doc(TypeInfo, second, 0),
+
+    file:delete(TempFile).
+
+parse_module(Code) ->
+    Lines = string:split(Code, "\n", all),
+    {Forms, _} = lists:foldl(
+        fun(Line, {Acc, LineNum}) ->
+            case string:trim(Line) of
+                "" ->
+                    {Acc, LineNum + 1};
+                TrimmedLine ->
+                    {ok, Tokens, _} = erl_scan:string(TrimmedLine ++ "\n", LineNum),
+                    {ok, Form} = erl_parse:parse_form(Tokens),
+                    {[Form | Acc], LineNum + 1}
+            end
+        end,
+        {[], 1},
+        Lines
+    ),
+    lists:reverse(Forms) ++ [{eof, 999}].
